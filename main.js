7a7fa169c9dbe2eb00e43c5e040271a099999cf3
shubham <shubhamv430@gmail.com>
2021-11-25 20:49:45 +0530

    added comments

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let realCtx = realCanvas.getContext('2d');
 ctx.fillStyle = "#ccc";
 realCtx.fillStyle = "#D99EAB";
 
+//  Creating multiple bars using loop with and making the bars equally from bottom my increasing the y according to height;
 let x = 0;
 for (let i = 0; i < 150; i++){
     
@@ -23,7 +24,7 @@ for (let i = 0; i < 150; i++){
     x = x + 7.5
 }
 
-//--------Function to Play and Pause Music----------
+//-----------Function to Play and Pause Music--------------
 let music = document.getElementById('music');
 let playBtn = document.getElementById('palyBtn');
 playBtn.addEventListener('click', playMusic);
@@ -48,7 +49,7 @@ inputRange.addEventListener('change', (e) => {
     
     music.currentTime = updatedTime;
 });
-
+//  on time updat we are show the bars color upto time in (%)
 music.ontimeupdate = function (e) {
     let currentTime = e.target.currentTime
     let duration = e.target.duration
@@ -60,7 +61,7 @@ music.onended = function () {
     playMusic();
 }
 
-
+// To mute and unmute audio----------------
 let sound = document.getElementById('volume');
 sound.addEventListener('click', soundChange);
 function soundChange() {
@@ -73,7 +74,7 @@ function soundChange() {
         sound.innerText='volume_up'
     }
 }
-
+// For skiping audion 10 seconds every time we click on fastForward button
 let fastForward = document.getElementById('fastFrBtn');
 
 fastForward.addEventListener('click', skipTime);
